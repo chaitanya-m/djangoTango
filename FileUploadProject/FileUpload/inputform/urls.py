05e515d4d364d14a775d1903ac3dd1291058de98
chaitanya-m <c.manapragada@gmail.com>
2014-07-26 07:30:48 +1000

    Form with input for user name

--- FileUploadProject/FileUpload/inputform/urls.py
+++ FileUploadProject/FileUpload/inputform/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 from inputform import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^add_username/$', views.add_username, name='add_username'),)
--- FileUploadProject/FileUpload/inputform/views.py
+++ FileUploadProject/FileUpload/inputform/views.py
@@ -1,6 +1,19 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
 from inputform.forms import UserNameForm
 
 def index(request):
-	return HttpResponse("Hello World!")
+    return HttpResponse("Hello World!")
+
+def add_username(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    form = UserNameForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('inputform/add_username.html', {'form': form}, context)
+	
