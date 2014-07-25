cda0c055b2d881364f79aa99177711009c432f3e
chaitanya-m <c.manapragada@gmail.com>
2014-07-25 17:00:30 +1000

    STATIC_PATH is simply the path to the 'static' folder, which contains static media such as images
    STATIC_URL is the url for web access to static media
    
    STATICFILES_DIRS is a variable for settings.py (the project settings file) that should be set to the location of the static media folder
    
    Tested- static media can be accessed on the dev server

--- FileUploadProject/FileUpload/FileUpload/settings.py
+++ FileUploadProject/FileUpload/FileUpload/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.join(BASE_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -90,4 +91,7 @@ MEDIA_URL = '/media/'
 
 TEMPLATE_DIRS = (
 	TEMPLATE_PATH,
+)
+STATICFILES_DIRS = (
+    STATIC_PATH,
 )
