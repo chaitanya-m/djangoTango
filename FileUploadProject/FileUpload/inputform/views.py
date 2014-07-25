f556c1f852ea4054c2366b693216d71ab818aeb7
chaitanya-m <c.manapragada@gmail.com>
2014-07-26 08:32:54 +1000

    Adding the 'inputform' to the list of installed apps is essential for creating the relevant database tables!
    At this stage, it is possible to post username data, and admin can look it up.

--- FileUploadProject/FileUpload/inputform/views.py
+++ FileUploadProject/FileUpload/inputform/views.py
@@ -10,8 +10,21 @@ def index(request):
 def add_username(request):
     # Get the context from the request.
     context = RequestContext(request)
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = UserNameForm(request.POST)
 
-    form = UserNameForm()
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            return index(request)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+        else:
+            return form.errors
+    else:
+        form = UserNameForm()
 
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
